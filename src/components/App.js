e419feaa2dedcd624566bb3e8dbe6e14d695cf66
Sam Avery <sam.avery1@gmail.com>
2022-09-25 13:43:04 -0500

    commit tree 97ffe10585861e9496e632246071af7c12858283

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, {useEffect, useState} from "react";
+
+
+function App() {
+    const [dog, setDog] = useState(null)
+
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+          setDog(data.message);
+          console.log(data)
+
+        });
+    }, []);
+
+
+    if (!dog) return <p>...Loading</p>
+    return <img alt="A Random Dog" src={dog}/>
+ }
+
+
+
+
+export default App
